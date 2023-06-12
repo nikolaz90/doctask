9c145a6d314bc89af75e9c0b03a38acfd3ce19f2
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-06-13 00:42:13 +0200

    starting to address test 2

--- app/models/concerns/events/scopable.rb
+++ app/models/concerns/events/scopable.rb
@@ -3,7 +3,12 @@ module Events
     extend ActiveSupport::Concern
 
     included do
-      scope :openings, -> { where(kind: 'opening') }
+      scope :openings, -> {
+        where(kind: 'opening')
+      }
+      scope :openings_today, ->(start_time) {
+        where(kind: 'opening').and(where('starts_at BETWEEN ? AND ?', start_time, start_time.end_of_day))
+      }
       scope :appointments_today, ->(start_time) {
         where(kind: 'appointment').and(where('starts_at BETWEEN ? AND ?', start_time, start_time.end_of_day))
       }
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,27 +2,40 @@ class Event < ApplicationRecord
   include Events::Scopable
 
   def self.availabilities(date_time)
-    p openings
+    p openings.map(&:availability)
+  end
+
+  def self.slots(starts_at, ends_at)
+    number_of_half_hours = ((ends_at - starts_at) / 1.hour).floor * 2
+    number_of_half_hours.times.map { |i| starts_at + i * 30.minutes }
   end
 
   def booked_slots
     return 'this is an opening' if kind == 'opening'
 
-    Event.appointments_for_day(starts_at)
+    slots_show_time
   end
 
   def open_slots
-    number_of_half_hours = ((ends_at - starts_at) / 1.hour).floor * 2
-    number_of_half_hours.times.map { |i| starts_at + i * 30.minutes }
-  end
+    return 'this is an appointment' if kind == 'appointment'
 
-  def slots_show_time(slots = open_slots)
+    slots = Event.slots(starts_at, ends_at)
+    bookings = Event.appointments_today(starts_at).map(&:booked_slots).flatten
+    slots.reject! { |date_time| bookings.include?(date_time.strftime('%H:%M')) }
+
     slots.map { |date_time| date_time.strftime('%H:%M') }
   end
 
   def availability
     return 'this is an appointment' if kind == 'appointment'
 
-    { date: starts_at.strftime('%d/%m/%Y'), slots: slots_show_time }
+    { date: starts_at.to_date, slots: open_slots }
+  end
+
+  private
+
+  def slots_show_time
+    slots = Event.slots(starts_at, ends_at)
+    slots.map { |date_time| date_time.strftime('%H:%M') }
   end
 end
