b07b4ff0f5dd39ec68238e706332e0ed7d91a368
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-06-12 22:37:38 +0200

    added slot show time method

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,20 @@
 class Event < ApplicationRecord
-
-include Events::Scopable
+  include Events::Scopable
 
   def self.availabilities(date_time)
     p openings
   end
 
-  private
-
   def open_slots
-    (starts_at.strftime("%H:%M")..ends_at.strftime("%H:%M"))
+    number_of_half_hours = ((ends_at - starts_at) / 1.hour).floor * 2
+    number_of_half_hours.times.map { |i| starts_at + i * 30.minutes }
   end
 
-  def self.availability(date)
-    {date: date, slots: []}
+  def slots_show_time(slots = open_slots)
+    slots.map { |date_time| date_time.strftime('%H:%M') }
+  end
+
+  def availability
+    { date: starts_at.strftime('%d/%m/%Y'), slots: slots_show_time }
   end
 end
