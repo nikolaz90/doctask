56ad0e8eca377f419f620906d8a45f4bd9c44506
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-06-14 23:19:55 +0200

    removed unecessary booked slots methode

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,16 +12,10 @@ class Event < ApplicationRecord
     end
   end
 
-  def booked_slots
-    return 'this is an opening' if kind == 'opening'
-
-    slots_show_time
-  end
-
   def open_slots(availability_date)
     return 'this is an appointment' if kind == 'appointment'
 
-    bookings = Event.appointments_today(availability_date).map(&:booked_slots).flatten
+    bookings = Event.appointments_today(availability_date).map(&:slots_show_time).flatten
     filtered_slots = slots.reject! { |date_time| bookings.include?(date_time.strftime('%H:%M')) }
     filtered_slots.map { |date_time| date_time.strftime('%H:%M') }
   end
@@ -42,17 +36,16 @@ class Event < ApplicationRecord
     starts_at.wday == date.wday && open_slots(date).any?
   end
 
+  def slots_show_time
+    slots.map { |date_time| date_time.strftime('%H:%M') }
+  end
+
   private
 
   def slots
     number_of_half_hours = ((ends_at - starts_at) / 1.hour).floor * 2
     number_of_half_hours.times.map { |i| starts_at + i * 30.minutes }
   end
-
-  def slots_show_time
-    slots.map { |date_time| date_time.strftime('%H:%M') }
-  end
-
   # def self.round_down(starts_at)
   #   Time.at((starts_at.to_f / 30.minutes).round * 30.minutes)
   # end
