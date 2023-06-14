6689ed34f158d3a8f4451b7a142c03a9d7b8d1c5
Nikolaz Stacey <nikolazstacey@hotmail.co.uk>
2023-06-14 23:36:21 +0200

    refactored appt today scope

--- app/models/concerns/events/scopable.rb
+++ app/models/concerns/events/scopable.rb
@@ -20,7 +20,13 @@ module Events
       }
 
       scope :appointments_today, ->(start_time) {
-        where(kind: 'appointment').and(where('starts_at BETWEEN ? AND ?', start_time, start_time.end_of_day))
+        where(
+          kind: 'appointment'
+        ).and(
+          where(
+            'starts_at BETWEEN ? AND ?', start_time, start_time.end_of_day
+            )
+          )
       }
     end
   end
